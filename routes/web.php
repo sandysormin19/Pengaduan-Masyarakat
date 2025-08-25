cfa9467fc6c43c7e373ea2f09932bbdb6ff6bb1f
sandysormin19 <sandysormin9@gmail.com>
2025-08-25 08:51:14 +0700

    Penambahan pada layouts auth dan page pada login serta route login.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/login', function () {
+    return view('pages.auth.login');
+});
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', function () {
